3d6201bffa66f1de67604d6ce0bd086037779253
Rimantas <belauzas@yahoo.com>
2020-10-02 12:00:32 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log(5, '+', 2, '=', 5 + 2);
+console.log((10 + 10 + 7) / 3);
+
+
+const paz1 = 10;
+const paz2 = 5;
+const paz3 = 7;
+const pazKiekis = 3;
+
+const vidurkis = (paz1 + paz2 + paz3) / pazKiekis;
+
+console.log(vidurkis);
